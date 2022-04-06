9cc60c415e26758ec0763976345762958148f1b8
0918nobita <nobita.0918@gmail.com>
2022-04-06 19:24:53 +0900

    :sparkles: /timeline エンドポイントを利用してテーブルの内容を表示するようにした

--- public/index.js
+++ public/index.js
@@ -4,6 +4,12 @@ const messageBody = document.getElementById('messageBody');
 const submitButton = document.getElementById('submitButton');
 const timeline = document.getElementById('timeline');
 
-const msgElement = document.createElement('div');
-msgElement.textContent = 'Saluton!';
-timeline.appendChild(msgElement);
+void (async () => {
+    const res = await fetch('/timeline');
+    const comments = await res.json();
+    for (const comment of comments) {
+        const msgElement = document.createElement('div');
+        msgElement.textContent = comment;
+        timeline.appendChild(msgElement);
+    }
+})();

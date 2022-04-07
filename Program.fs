b523f815afd318e76a113e62ae59c5b0a03ffb2b
0918nobita <nobita.0918@gmail.com>
2022-04-07 13:23:26 +0900

    :recycle: Do refactor

--- Program.fs
+++ Program.fs
@@ -1,6 +1,7 @@
 module Program
 
 open System
+open System.Data.SQLite
 open FSharp.Json
 open Suave
 open Suave.Filters
@@ -8,46 +9,63 @@ open Suave.Operators
 
 let webServerConfig: SuaveConfig =
     let ip = Net.IPAddress.Parse("0.0.0.0")
-    let httpBinding = {
-        scheme = HTTP
-        socketBinding = {
-            ip = ip
-            port = 8080us
-        }
-    }
+
+    let httpBinding =
+        { scheme = HTTP
+          socketBinding = { ip = ip; port = 8080us } }
+
     let homeFolder = IO.Path.GetFullPath("./public")
-    {
-        defaultConfig with
-            bindings = [httpBinding]
-            homeFolder = Some homeFolder
-    }
+
+    { defaultConfig with
+        bindings = [ httpBinding ]
+        homeFolder = Some homeFolder }
+
+let sendJson data =
+    Successful.OK(Json.serialize data)
+    >=> Writers.setMimeType "application/json"
+
+/// SQLite データベースを操作する
+///
+/// 成功したらコールバックの戻り値 `v` が `Some v` の形式で返り、失敗したら `None` が返る
+let useSQLiteConn (dataSource: string) (f: SQLiteConnection -> 'a) : 'a option =
+    let connStr =
+        SQLiteConnectionStringBuilder(DataSource = dataSource)
+            .ToString()
+
+    use conn = new SQLiteConnection(connStr)
+
+    try
+        conn.Open()
+        let webPart = f conn
+        conn.Close()
+        Some webPart
+    with
+    | :? SQLiteException -> None
+
+let timeline =
+    GET
+    >=> path "/timeline"
+    >=> warbler (fun _ctx ->
+        useSQLiteConn "db.sqlite" (fun conn ->
+            use cmd = conn.CreateCommand()
+            cmd.CommandText <- "SELECT * From timeline"
+            cmd.Prepare()
+            let reader = cmd.ExecuteReader()
+            let mutable comments = []
+
+            while reader.Read() do
+                comments <- reader.GetString(1) :: comments
+
+            sendJson comments)
+        |> Option.orDefault (fun () -> ServerErrors.INTERNAL_ERROR "Something went wrong while collecting comments"))
 
 let app =
-    choose [
-        GET >=> path "/" >=> Files.browseFileHome "index.html"
-        GET
-            >=> path "/timeline"
-            >=> warbler (fun _ctx ->
-                let connStr = Data.SQLite.SQLiteConnectionStringBuilder(DataSource = "db.sqlite").ToString()
-                use conn = new Data.SQLite.SQLiteConnection(connStr)
-                try
-                    conn.Open()
-                    use cmd = conn.CreateCommand()
-                    cmd.CommandText <- "SELECT * from timeline"
-                    cmd.Prepare()
-                    use reader = cmd.ExecuteReader()
-                    let mutable comments = []
-                    while reader.Read() do
-                        comments <- reader.GetString(1)::comments
-                    conn.Close()
-                    Successful.OK (Json.serialize comments) >=> Writers.setMimeType "application/json"
-                with
-                | :? Data.SQLite.SQLiteException ->
-                    ServerErrors.INTERNAL_ERROR "Something went wrong while collecting comments"
-            )
-        GET >=> Files.browseHome
-        RequestErrors.NOT_FOUND "Page not found"
-    ]
+    choose [ GET
+             >=> path "/"
+             >=> Files.browseFileHome "index.html"
+             timeline
+             GET >=> Files.browseHome
+             RequestErrors.NOT_FOUND "Page not found" ]
 
 [<EntryPoint>]
 let main _args =
